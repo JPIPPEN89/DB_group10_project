447d6f623ea32d917f172629ee810ccf5d73f21d
JOSHP <JOSHpippen89@icloud.com>
2026-04-17 13:19:38 -0500

    added uncharted

--- Group10_Project3.sql
+++ Group10_Project3.sql
@@ -255,3 +255,605 @@ INSERT INTO relatives VALUES ('Sibling Name', 1);
 INSERT INTO stars_photos VALUES ('photo1.jpg', 1);
 
 INSERT INTO industry_person_awards VALUES ('Best Director', 1);
+
+--================================================================
+--          AQUAMAN
+--==============================================================
+
+
+    INSERT INTO movie VALUES (
+    2, 'Aquaman and the Lost Kingdom', 2362, DATE '2023-12-22',
+    'United Kingdom', 40.4,20.5 ,124 , 'London, England'
+);
+
+INSERT INTO genre VALUES (2, 'Action');
+
+INSERT INTO belongs_to VALUES (2, 2);
+
+INSERT INTO stars VALUES (
+    2,
+    'Patrick Wilson',
+    DATE '1973-07-03',
+    185,
+    'Bio placeholder',
+    NULL
+);
+
+INSERT INTO acts_in VALUES (2, 1); -- Jason Momoa (already exists)
+INSERT INTO acts_in VALUES (2, 2); -- Patrick Wilson
+
+INSERT INTO industry_person VALUES (
+    4,
+    NULL,
+    NULL,
+    NULL,
+    'James Wan',
+    'Director'
+);
+
+INSERT INTO director VALUES (4, 'DGA');
+
+INSERT INTO has_credit VALUES (2, 4);
+
+INSERT INTO movie_languages VALUES ('English', 2);
+
+INSERT INTO production_companies VALUES ('Warner Bros', 2);
+
+INSERT INTO reviews VALUES (
+    2,
+    56,
+    'Placeholder review',
+    2
+);
+
+
+
+-- =========================
+-- Uncharted
+-- =========================
+
+INSERT INTO movie VALUES (
+    3,
+    'Uncharted',
+    735,
+    DATE '2022-18-02',
+    'Spain',
+    407,
+    120,
+    116,
+    'Lloret de Mar, Girona, Spain'
+);
+
+INSERT INTO genre VALUES (
+    3,
+    'Adventure'
+);
+
+INSERT INTO stars VALUES (
+    3,
+    'Tom Holland',
+    DATE '1996-06-01',
+    66,
+    'Thomas Stanley Holland was born in Kingston-upon-Thames, Surrey, to Nicola Elizabeth (Frost)...',
+    NULL
+);
+
+INSERT INTO industry_person VALUES (
+    5,
+    DATE '1974-31-10',
+    'Washington, District of Columbia, USA',
+    'Ruben Fleischer was born on October 31, 1974 in Washington, District of Columbia, USA...',
+    'Ruben Fleishcer',
+    'Directing Zombie Land'
+);
+
+INSERT INTO industry_person VALUES (
+    6,
+    DATE '<1983-01-08>',
+    '<birth_place>',
+    'Rafe Judkins was born on January 8, 1983 in Salt Lake City, Utah, USA. He is a producer and writer...',
+    'Rafe Judkins',
+    'Wheel of Time, Uncharted'
+);
+
+
+
+INSERT INTO director VALUES (
+    5,
+    '<guild_membership>'
+);
+
+INSERT INTO writer VALUES (
+    6,
+    '<genre_speciality>'
+);
+
+-- =========================
+-- DEPENDENT TABLES
+-- =========================
+
+INSERT INTO movie_languages VALUES (
+    'Spanish',
+    3
+);
+
+INSERT INTO movie_languages VALUES (
+    'English',
+    3
+);
+
+INSERT INTO production_companies VALUES (
+    'Columbia Pictures',
+    3
+);
+
+INSERT INTO production_companies VALUES (
+    'Arad Productions',
+    3
+);
+
+INSERT INTO reviews VALUES (
+    <review_id>,
+    <review_score>,
+    '<review>',
+    <movie_ID>
+);
+
+-- =========================
+-- JUNCTION TABLES
+-- =========================
+
+INSERT INTO belongs_to VALUES (
+    3,
+    3
+);
+
+INSERT INTO acts_in VALUES (
+    3,
+    3
+);
+
+INSERT INTO writes VALUES (
+    3,
+    6
+);
+
+INSERT INTO has_credit VALUES (
+    3,
+    6
+);
+
+INSERT INTO has_credit VALUES (
+    3,
+    5
+);
+
+INSERT INTO cast_by VALUES (
+    3,
+    5
+);
+
+INSERT INTO cast_by VALUES (
+    3,
+    6
+);
+
+-- =========================
+-- EXTRA RELATION TABLES
+-- =========================
+
+INSERT INTO stars_other_works VALUES (
+    'Billy Elliot: The Musical',
+    3
+);
+
+INSERT INTO relatives VALUES (
+    'Harry Hollan (sibling)',
+    3
+);
+
+
+---------------------------------------------------------------------------
+
+-- =========================
+-- BASE TABLES
+-- =========================
+
+INSERT INTO movie VALUES (
+    <movie_ID>,
+    '<movie_name>',
+    <popularity>,
+    DATE '<YYYY-MM-DD>',
+    '<country_of_origin>',
+    <income>,
+    <production_cost>,
+    <duration>,
+    '<filming_locations>'
+);
+
+INSERT INTO genre VALUES (
+    <genre_id>,
+    '<genre_name>'
+);
+
+INSERT INTO stars VALUES (
+    <star_id>,
+    '<star_name>',
+    DATE '<YYYY-MM-DD>',
+    <height>,
+    '<biography>',
+    '<alt_names>'
+);
+
+INSERT INTO industry_person VALUES (
+    <industry_person_id>,
+    DATE '<YYYY-MM-DD>',
+    '<birth_place>',
+    '<biography>',
+    '<name>',
+    '<known_for>'
+);
+
+-- =========================
+-- SUBTYPE TABLES
+-- =========================
+
+INSERT INTO producer VALUES (
+    <industry_person_id>,
+    '<production_company>'
+);
+
+INSERT INTO director VALUES (
+    <industry_person_id>,
+    '<guild_membership>'
+);
+
+INSERT INTO writer VALUES (
+    <industry_person_id>,
+    '<genre_speciality>'
+);
+
+-- =========================
+-- DEPENDENT TABLES
+-- =========================
+
+INSERT INTO movie_languages VALUES (
+    '<language>',
+    <movie_ID>
+);
+
+INSERT INTO production_companies VALUES (
+    '<production_company>',
+    <movie_ID>
+);
+
+INSERT INTO reviews VALUES (
+    <review_id>,
+    <review_score>,
+    '<review>',
+    <movie_ID>
+);
+
+-- =========================
+-- JUNCTION TABLES
+-- =========================
+
+INSERT INTO belongs_to VALUES (
+    <genre_id>,
+    <movie_ID>
+);
+
+INSERT INTO acts_in VALUES (
+    <movie_ID>,
+    <star_id>
+);
+
+INSERT INTO writes VALUES (
+    <movie_ID>,
+    <industry_person_id>
+);
+
+INSERT INTO has_credit VALUES (
+    <movie_ID>,
+    <industry_person_id>
+);
+
+INSERT INTO cast_by VALUES (
+    <star_id>,
+    <industry_person_id>
+);
+
+-- =========================
+-- EXTRA RELATION TABLES
+-- =========================
+
+INSERT INTO stars_other_works VALUES (
+    '<work>',
+    <star_id>
+);
+
+INSERT INTO relatives VALUES (
+    '<relative_name>',
+    <star_id>
+);
+
+INSERT INTO stars_photos VALUES (
+    '<photo_path>',
+    <star_id>
+);
+
+INSERT INTO industry_person_awards VALUES (
+    '<award>',
+    <industry_person_id>
+);
+
+
+---------------------------------------------------------
+
+-- =========================
+-- BASE TABLES
+-- =========================
+
+INSERT INTO movie VALUES (
+    <movie_ID>,
+    '<movie_name>',
+    <popularity>,
+    DATE '<YYYY-MM-DD>',
+    '<country_of_origin>',
+    <income>,
+    <production_cost>,
+    <duration>,
+    '<filming_locations>'
+);
+
+INSERT INTO genre VALUES (
+    <genre_id>,
+    '<genre_name>'
+);
+
+INSERT INTO stars VALUES (
+    <star_id>,
+    '<star_name>',
+    DATE '<YYYY-MM-DD>',
+    <height>,
+    '<biography>',
+    '<alt_names>'
+);
+
+INSERT INTO industry_person VALUES (
+    <industry_person_id>,
+    DATE '<YYYY-MM-DD>',
+    '<birth_place>',
+    '<biography>',
+    '<name>',
+    '<known_for>'
+);
+
+-- =========================
+-- SUBTYPE TABLES
+-- =========================
+
+INSERT INTO producer VALUES (
+    <industry_person_id>,
+    '<production_company>'
+);
+
+INSERT INTO director VALUES (
+    <industry_person_id>,
+    '<guild_membership>'
+);
+
+INSERT INTO writer VALUES (
+    <industry_person_id>,
+    '<genre_speciality>'
+);
+
+-- =========================
+-- DEPENDENT TABLES
+-- =========================
+
+INSERT INTO movie_languages VALUES (
+    '<language>',
+    <movie_ID>
+);
+
+INSERT INTO production_companies VALUES (
+    '<production_company>',
+    <movie_ID>
+);
+
+INSERT INTO reviews VALUES (
+    <review_id>,
+    <review_score>,
+    '<review>',
+    <movie_ID>
+);
+
+-- =========================
+-- JUNCTION TABLES
+-- =========================
+
+INSERT INTO belongs_to VALUES (
+    <genre_id>,
+    <movie_ID>
+);
+
+INSERT INTO acts_in VALUES (
+    <movie_ID>,
+    <star_id>
+);
+
+INSERT INTO writes VALUES (
+    <movie_ID>,
+    <industry_person_id>
+);
+
+INSERT INTO has_credit VALUES (
+    <movie_ID>,
+    <industry_person_id>
+);
+
+INSERT INTO cast_by VALUES (
+    <star_id>,
+    <industry_person_id>
+);
+
+-- =========================
+-- EXTRA RELATION TABLES
+-- =========================
+
+INSERT INTO stars_other_works VALUES (
+    '<work>',
+    <star_id>
+);
+
+INSERT INTO relatives VALUES (
+    '<relative_name>',
+    <star_id>
+);
+
+INSERT INTO stars_photos VALUES (
+    '<photo_path>',
+    <star_id>
+);
+
+INSERT INTO industry_person_awards VALUES (
+    '<award>',
+    <industry_person_id>
+);
+
+
+---------------------------------------------------------
+
+-- =========================
+-- BASE TABLES
+-- =========================
+
+INSERT INTO movie VALUES (
+    <movie_ID>,
+    '<movie_name>',
+    <popularity>,
+    DATE '<YYYY-MM-DD>',
+    '<country_of_origin>',
+    <income>,
+    <production_cost>,
+    <duration>,
+    '<filming_locations>'
+);
+
+INSERT INTO genre VALUES (
+    <genre_id>,
+    '<genre_name>'
+);
+
+INSERT INTO stars VALUES (
+    <star_id>,
+    '<star_name>',
+    DATE '<YYYY-MM-DD>',
+    <height>,
+    '<biography>',
+    '<alt_names>'
+);
+
+INSERT INTO industry_person VALUES (
+    <industry_person_id>,
+    DATE '<YYYY-MM-DD>',
+    '<birth_place>',
+    '<biography>',
+    '<name>',
+    '<known_for>'
+);
+
+-- =========================
+-- SUBTYPE TABLES
+-- =========================
+
+INSERT INTO producer VALUES (
+    <industry_person_id>,
+    '<production_company>'
+);
+
+INSERT INTO director VALUES (
+    <industry_person_id>,
+    '<guild_membership>'
+);
+
+INSERT INTO writer VALUES (
+    <industry_person_id>,
+    '<genre_speciality>'
+);
+
+-- =========================
+-- DEPENDENT TABLES
+-- =========================
+
+INSERT INTO movie_languages VALUES (
+    '<language>',
+    <movie_ID>
+);
+
+INSERT INTO production_companies VALUES (
+    '<production_company>',
+    <movie_ID>
+);
+
+INSERT INTO reviews VALUES (
+    <review_id>,
+    <review_score>,
+    '<review>',
+    <movie_ID>
+);
+
+-- =========================
+-- JUNCTION TABLES
+-- =========================
+
+INSERT INTO belongs_to VALUES (
+    <genre_id>,
+    <movie_ID>
+);
+
+INSERT INTO acts_in VALUES (
+    <movie_ID>,
+    <star_id>
+);
+
+INSERT INTO writes VALUES (
+    <movie_ID>,
+    <industry_person_id>
+);
+
+INSERT INTO has_credit VALUES (
+    <movie_ID>,
+    <industry_person_id>
+);
+
+INSERT INTO cast_by VALUES (
+    <star_id>,
+    <industry_person_id>
+);
+
+-- =========================
+-- EXTRA RELATION TABLES
+-- =========================
+
+INSERT INTO stars_other_works VALUES (
+    '<work>',
+    <star_id>
+);
+
+INSERT INTO relatives VALUES (
+    '<relative_name>',
+    <star_id>
+);
+
+INSERT INTO stars_photos VALUES (
+    '<photo_path>',
+    <star_id>
+);
+
+INSERT INTO industry_person_awards VALUES (
+    '<award>',
+    <industry_person_id>
+);
+
